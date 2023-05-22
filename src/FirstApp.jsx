a56ebfb3979582ee6ab1718089ae0c6773d65f38
illika <steve.milan7@gmail.com>
2023-05-21 22:29:55 -0500

    Parametros

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,7 +1,18 @@
+const newMessage = {
+    message: "Hola Mundo",
+    title: "Illika"
+};
+
+const nombre = () => {
+    return "illika";
+}
+
 export const FirstApp = () => {
+
     return (
         <>
-            <h1>Illika</h1>
+            <h1>{ nombre() }</h1>
+            <code>{ JSON.stringify(newMessage) }</code>
             <p>Soy un Subtitulo</p>
         </>
     );
