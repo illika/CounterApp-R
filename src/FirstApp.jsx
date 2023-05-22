73074eb63bcecd75caef2f0f9c13a8864b08ae17
illika <steve.milan7@gmail.com>
2023-05-21 23:03:48 -0500

    prop-types | defaultProps

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,8 +1,23 @@
-export const FirstApp = ({title, subtitle}) => {
+import PropTypes from "prop-types"
+
+export const FirstApp = ({title, subTitle, name}) => {
+
     return (
         <>
             <h1>{ title }</h1>
-            <p>{ subtitle + 1 }</p>
+            <p>{ subTitle }</p>
+            <p>{ name } </p>
         </>
     );
+}
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.string
+}
+
+FirstApp.defaultProps = {
+    title: "No hay titulo",
+    subTitle: "No hay subTitle",
+    name: "illika"
 }
