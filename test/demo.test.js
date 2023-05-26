de7e166cbe1e631a0b81e87c40fa1af73955d507
illika <steve.milan7@gmail.com>
2023-05-25 23:12:31 -0500

    describe - expect().toBe()

--- test/demo.test.js
+++ test/demo.test.js
@@ -1,5 +1,14 @@
-test('Esta prueba no debe fallar', () => {
-    if(1 === 0) {
-        throw new Error('No puede dividir entre cero');
-    }
-})
+describe('Pruebas en <DemoComponent />', () => {
+    test('Esta prueba no debe fallar', () => {
+
+        // 1. Inicializamos
+        const message1 = 'Hola Mundo';
+
+        // 2. Estimulo
+        const message2 = message1.trim();
+
+        // 3. Observar comportamiento
+        expect(message1).toBe(message2);
+
+    });
+});
