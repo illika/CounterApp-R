3da9347b7f76e61f90085b06df8064531603d718
illika <steve.milan7@gmail.com>
2023-05-28 00:19:33 -0500

    toMatchSnapshot() | toBeTruthy() | toContain()

--- test/FirstApp.test.jsx
+++ test/FirstApp.test.jsx
@@ -6,8 +6,22 @@ describe('Pruebas en <FirstApp />', () => {
     test('Debe hacer match con el snapshot', () => {
 
         const title = 'Hola, Soy Nilo';
-        render( <FirstApp title={title} /> )
+        const { container } = render(<FirstApp title={title} />);
 
+        expect(container).toMatchSnapshot();
+
+    });
+
+    test('Debe mostrar el titulo en h1', () => {
+
+        const title = 'Hola, Soy Nilo';
+        const { container, getByText } = render(<FirstApp title={title} />);
+
+        expect(getByText(title)).toBeTruthy();
+
+        // NO RECOMENDADO
+        const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toContain(title);
 
     });
 });
